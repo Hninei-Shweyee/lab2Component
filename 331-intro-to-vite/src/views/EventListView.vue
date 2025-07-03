6bb424f1f1fb86e439ec8a5b16669d99c1df7af6
Hnin Ei Shwe Yee <eishweyee3110@gmail.com>
2025-07-04 00:33:33 +0700

    15.4

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -10,7 +10,7 @@ onMounted(() => {
   axios
     .get('https://my-json-server.typicode.com/Hninei-Shweyee/mock-events/events')
     .then((response) => {
-      console.log(response.data)
+      events.value=response.data
     })
     .catch((error) => {
       console.error('There was an error!', error)
