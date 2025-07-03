265e30e400943b6fa98fca0f2c87895066cef496
Hnin Ei Shwe Yee <eishweyee3110@gmail.com>
2025-07-04 02:31:01 +0700

    16.2

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -2,13 +2,13 @@
 import EventMeta from '@/components/EventMeta.vue'
 import { Event } from '@/types'
 import { ref,onMounted } from 'vue'
-import axios from 'axios'
+import EventService from '@/services/EventService'
 
 const events=ref<Event[]>(null)
 
 onMounted(() => {
-  axios
-    .get('https://my-json-server.typicode.com/Hninei-Shweyee/mock-events/events')
+  
+    EventService.getEvents()
     .then((response) => {
       events.value=response.data
     })
